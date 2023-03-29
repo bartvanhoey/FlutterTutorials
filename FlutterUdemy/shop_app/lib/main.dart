868a08d46c8978e4c120abd13cd04e92e81eeef9
bartvanhoey <bartvanhoey@hotmail.com>
2023-03-29 20:57:33 +0200

    End Part 190

---
+++ FlutterUdemy/shop_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shop_app/screens/products_overview_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MyShop',
+      theme: ThemeData(
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.deepOrange)),
+      home: ProductsOverviewScreen(),
+    );
+  }
+}
