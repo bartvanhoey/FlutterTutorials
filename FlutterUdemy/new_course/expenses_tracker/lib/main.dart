bbe05e77767f9e844fe684ae1b9ea00ce531919d
bartvanhoey <bartvanhoey@hotmail.com>
2023-06-14 09:19:25 +0200

    Section 6: Lock Orientation

--- FlutterUdemy/new_course/expenses_tracker/lib/main.dart
+++ FlutterUdemy/new_course/expenses_tracker/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
@@ -9,49 +10,49 @@ var kDarkColorScheme = ColorScheme.fromSeed(
     seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
-  runApp(MaterialApp(
-    // theme: ThemeData(useMaterial3: true),
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) => runApp(MaterialApp(
+            // theme: ThemeData(useMaterial3: true),
 
-    darkTheme: ThemeData.dark().copyWith(
-      useMaterial3: true,
-      colorScheme: kDarkColorScheme,
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              foregroundColor: kDarkColorScheme.onPrimaryContainer,
-              backgroundColor: kDarkColorScheme.primaryContainer),
-              
-        ),
-      cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 8,
-          )),
-          
-    ),
-    theme: ThemeData().copyWith(
-        useMaterial3: true,
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-            backgroundColor: kColorScheme.onPrimaryContainer,
-            foregroundColor: kColorScheme.primaryContainer),
-        cardTheme: const CardTheme().copyWith(
-            color: kColorScheme.secondaryContainer,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 16,
-              vertical: 8,
-            )),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: kColorScheme.primaryContainer),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-            titleLarge: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: kColorScheme.onSecondaryContainer,
-                fontSize: 16))),
-    themeMode: ThemeMode.system,
+            darkTheme: ThemeData.dark().copyWith(
+              useMaterial3: true,
+              colorScheme: kDarkColorScheme,
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                    foregroundColor: kDarkColorScheme.onPrimaryContainer,
+                    backgroundColor: kDarkColorScheme.primaryContainer),
+              ),
+              cardTheme: const CardTheme().copyWith(
+                  color: kDarkColorScheme.secondaryContainer,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 8,
+                  )),
+            ),
+            theme: ThemeData().copyWith(
+                useMaterial3: true,
+                colorScheme: kColorScheme,
+                appBarTheme: const AppBarTheme().copyWith(
+                    backgroundColor: kColorScheme.onPrimaryContainer,
+                    foregroundColor: kColorScheme.primaryContainer),
+                cardTheme: const CardTheme().copyWith(
+                    color: kColorScheme.secondaryContainer,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 8,
+                    )),
+                elevatedButtonTheme: ElevatedButtonThemeData(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: kColorScheme.primaryContainer),
+                ),
+                textTheme: ThemeData().textTheme.copyWith(
+                    titleLarge: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: kColorScheme.onSecondaryContainer,
+                        fontSize: 16))),
+            themeMode: ThemeMode.system,
 
-    home: const Expenses(),
-  ));
+            home: const Expenses(),
+          )));
 }
